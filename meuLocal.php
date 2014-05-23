407ac5a43bb93ef41e469f997d3e0e8786206386
felipefurlann <felipe@planow.com.br>
2014-05-23 14:32:22 -0300

    first commit

---
+++ meuLocal.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="demo">CLIQUE NO BOTAO PARA OBTER AS CORRDENADAS</p>
+<button onclick="getLocation()">CLIQUE AQUI</button>
+<script>
+var x=document.getElementById("demo");
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    }
+  else{x.innerHTML="Geolocation is not supported by this browser.";}
+  }
+function showPosition(position)
+  {
+  x.innerHTML="Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;	
+  }
+</script>
+</body>
+</html>
